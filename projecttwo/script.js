7cd97a2bb72ea758a59b02951eda640d1600da35
Damian Ciasnocha <daneek80@gmail.com>
2019-08-30 21:48:57 +0100

    Add files via upload

---
+++ projecttwo/script.js
@@ -0,0 +1,9 @@
+// lets start with selecting elements
+// lets create Array
+const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+// lets select button
+const hexBtn = document.querySelector('.hexBtn');
+// let say what needs to happend after press click-background needs to change
+const bodyBgc = document.querySelector('body');
+// let select span
+const hex = document.querySelector('.hex');
